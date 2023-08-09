3f40f06abd0436e18decccd3b1d8fc320227fa6e
Natalia <natalia.semyonova@gmail.com>
2023-08-09 19:54:58 +0300

    Modified code

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -29,14 +29,14 @@ public class MoneyTransferTest {
         val initialBalanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
         val cardInfoSecond = DataHelper.getSecondCard();
         val initialBalanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
-        int  randomSum = DataHelper.getSumForTopUpFirstCard();
+        int randomSum = DataHelper.getSumForTopUpFirstCard();
         cardsPage.topUpFirstCard();
-        val transferPage =new TransferPage();
+        val transferPage = new TransferPage();
         transferPage.topUp(randomSum);
         val balanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
         val balanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
         int expectedFirst = initialBalanceFirst + randomSum;
-        int expectedSecond= initialBalanceSecond - randomSum;
+        int expectedSecond = initialBalanceSecond - randomSum;
         assertEquals(expectedFirst, balanceFirst);
         assertEquals(expectedSecond, balanceSecond);
     }
@@ -54,40 +54,16 @@ public class MoneyTransferTest {
         val initialBalanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
         val cardInfoSecond = DataHelper.getSecondCard();
         val initialBalanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
-        int  randomSum = DataHelper.getSumForTopUpSecondCard();
+        int randomSum = DataHelper.getSumForTopUpSecondCard();
         cardsPage.topUpSecondCard();
-        val transferPage =new TransferPage();
+        val transferPage = new TransferPage();
         transferPage.topUp(randomSum);
         val balanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
         val balanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
         int expectedFirst = initialBalanceFirst - randomSum;
-        int expectedSecond= initialBalanceSecond + randomSum;
+        int expectedSecond = initialBalanceSecond + randomSum;
         assertEquals(expectedFirst, balanceFirst);
         assertEquals(expectedSecond, balanceSecond);
     }
-
-    @Test
-    void shouldNotTransferIfBalanseNotEnough() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
-        val cardsPage = new CardsPage();
-        val cardInfoFirst = DataHelper.getFirstCard();
-        val initialBalanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
-        val cardInfoSecond = DataHelper.getSecondCard();
-        val initialBalanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
-        int  randomSum = DataHelper.getInvalidSumForTopUpSecondCard();
-        cardsPage.topUpSecondCard();
-        val transferPage =new TransferPage();
-        transferPage.topUp(randomSum);
-        val balanceFirst = new DashboardPage().getCardBalance(cardInfoFirst);
-        val balanceSecond = new DashboardPage().getCardBalance(cardInfoSecond);
-        int expectedFirst = initialBalanceFirst - randomSum;
-        int expectedSecond= initialBalanceSecond + randomSum;
-        Assertions.assertFalse(expectedFirst < 0);
-        assertEquals(initialBalanceSecond, balanceSecond);
-    }
 }
+
